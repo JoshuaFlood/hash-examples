f9362f3cde360ae9ddf6a38a6736183867e8736e
Joshua Flood <info@joshuaflood.com>
2021-04-16 00:12:03 +0100

    Update sha256.php

--- sha256.php
+++ sha256.php
@@ -49,30 +49,26 @@ $salt = $timestamp . getRandomString(256);
 ///
 
 // Once we have our salt, we attach it as a prefix to the user's password and pass it as a parameter to the `hash()` function.
-
 // The first argument of the `hash()` function is a string which defines the algorithm used in hashing. The second argument is the data to be hashed.
 
 $saltPasswordCombination = hash('sha256', $salt . 'Password123');
+
 
 ///
 // Additional notes
 ///
 
-
 // It is important to remember that this code should only ever be executed client-side, and the user's password should never be transmitted over the internet.
 
 // In a production environment, the salt would only be generated when a user creates a new account.
-
 // The salt would then be stored in a database alongside their username and the value of the `$saltPasswordCombination` variable above,
 // but the database would not store the user's password.
 
 // Whenever an existing user attempts to login to the site, the client makes a request to the server and retrieves the salt from the database.
-
-// The client executes the `hash()` function, using the salt from the database in place of the `$salt` variable in the above example,
-// and retrieves the value of the `$password` variable from the user via the login form.
-
-// The result of this hash is then sent back to the server and the server compares the new salt-password combination against
-// the hash that is stored in the database.
-
+// The client executes the `hash()` function, using the salt from the database in place of the `$salt` variable in the above example.
+// The resulting hash is then sent back to the server where it is compared against the hash that was stored in the database when the user created their account.
 // If both hashes are the same, then the user must have provided the correct password and is allowed access to the site.
 // If the hashes are not the same, then the user must have provided an incorrect password and is not granted access to the site.
+
+// If a user wishes to change their password, a new value is generated using the same salt and the new password and the resulting hash replaces the
+// hash that was previously stored in the databas, to be compared against future login attempts.
